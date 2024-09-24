ceecd5d99af191bf5a7979f323caea9664fd2a43
GitOnat <maon0001@stud.kea.dk>
2024-09-24 13:31:00 +0200

    Initial commit

---
+++ src/main/java/com/example/hello_github_actions_demo/HelloGithubActionsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello_github_actions_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGithubActionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGithubActionsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hello_github_actions_demo/HelloGithubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_github_actions_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGithubActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
